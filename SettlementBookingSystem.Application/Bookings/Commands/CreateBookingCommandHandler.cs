920938ebdd395dc62d4ed3a2a43cdedd2b7f1271
tuannguyen1303 <tuan1331210006@gmail.com>
2022-07-31 09:22:52 +0700

    firt commit source

---
+++ SettlementBookingSystem.Application/Bookings/Commands/CreateBookingCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using SettlementBookingSystem.Application.Bookings.Dtos;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SettlementBookingSystem.Application.Bookings.Commands
+{
+    public class CreateBookingCommandHandler : IRequestHandler<CreateBookingCommand, BookingDto>
+    {
+        public CreateBookingCommandHandler()
+        {
+        }
+
+        public Task<BookingDto> Handle(CreateBookingCommand request, CancellationToken cancellationToken)
+        {
+            // TODO Implement CreateBookingCommandHandler.Handle() and confirm tests are passing. See InfoTrack Global Team - Tech Test.pdf for business rules.
+            throw new NotImplementedException();
+        }
+    }
+}
